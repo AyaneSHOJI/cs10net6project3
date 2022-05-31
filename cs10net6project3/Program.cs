9cc5b66ef75720639598e6d8950a8b39fed9352a
AyaneSHOJI <shojiayane@gmail.com>
2022-05-31 11:51:37 +0200

    finished recursion section

--- cs10net6project3/Program.cs
+++ cs10net6project3/Program.cs
@@ -1,8 +1,8 @@
 ﻿using static System.Console;
 
-//p.131 Chapter04 Writing, Debugging, and Testing Functions
+// p.131 Chapter04 Writing, Debugging, and Testing Functions
 
-//p.132 Time table function
+// p.132 Time table function
 static void TimeTable(byte number)
 {
     WriteLine($"This is the {number} times table:");
@@ -15,13 +15,13 @@ static void TimeTable(byte number)
 
 TimeTable(6);
 
-//p.134 Function that returns a value
+// p.134 Function that returns a value
 static decimal CalculateTax(decimal amount, string twoLetterRegionCode)
 {
     decimal rate = 0.0M;
 
     switch(twoLetterRegionCode){
-        case "CH": //SwitzerLand
+        case "CH": // SwitzerLand
             rate = 0.08M;
             break;
         case "DK":
@@ -40,13 +40,13 @@ static decimal CalculateTax(decimal amount, string twoLetterRegionCode)
 
 decimal taxToPay = CalculateTax(149, "FR");
 WriteLine($"You must pay {taxToPay} in tax");
-//problems with this function, if a user mistakes letters of region ? 
+// problems with this function, if a user mistakes letters of region ? 
 
-//p.136 Converting cardinal to ordinal
+// p.136 Converting cardinal to ordinal
 static string CardinaltoOrdinal(int number)
 {
-    //specific cases
-    //switch statement
+    // specific cases
+    // switch statement
     switch (number)
     {
         case 11:
@@ -55,7 +55,7 @@ static string CardinaltoOrdinal(int number)
             return $"{number}th";
         default:
             int lastDigit = number % 10;
-            //switch expression
+            // switch expression
             string suffix = lastDigit switch
             {
                 1 => "st",
@@ -77,9 +77,10 @@ static void RunCardinalToOrdinal()
 }
 RunCardinalToOrdinal();
 
-//p.137 Calculation factoriales
+// p.137 Calculation factoriales
 static int Factorial(int number)
 {
+    // DO NOT FORGET to define the end of loop here
     if(number < 1)
     {
         return 0;
@@ -88,18 +89,62 @@ static int Factorial(int number)
     {
         return 1;
     }
-    else
+    checked
     {
         return number * Factorial(number - 1);
     }
+
+   
 }
 
 static void RunFactorial()
 {
-    for(int i =1; i <5; i++)
+    for(int i =1; i <15; i++)
     {
-        WriteLine($"{i}! = {Factorial(i):N0}");
+        try
+        {
+            WriteLine($"{i}! = {Factorial(i):N0}");
+        }
+        catch (System.OverflowException)
+        {
+            WriteLine($"The {i} is too big for a 32-bit integer. ");
+        }
+        
     }
 }
 
 RunFactorial();
+
+/* what's happend here ?
+ when n = 5
+5 * Factorial(4)
+then
+5 * 4 * Factorial(3)
+then
+5 * 4 * Factorial(2)
+then
+5 * 4 * 3 * Factorial(1)
+then
+5 * 4 * 3 * 2 * Factorial(1)
+at last
+5 * 4 * 3 * 2 * 1 
+
+------Recursion model-----
+returnType func(param) {
+  if (basecase) {
+    return valueBasecase;
+  }
+
+  func(nextParam); 
+  return value1;
+}
+
+the first function calls the second one, 
+the second one calls the third one...
+the last element returns back its value to the previous element and repeat
+
+cf. recursion.png
+cf. https://qiita.com/drken/items/23a4f604fa3f505dd5ad
+ */
+
+// p.140
